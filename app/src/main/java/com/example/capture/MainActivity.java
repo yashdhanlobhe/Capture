2008f5b177e09ad55007347d0d9d2d35f7772d59
billion-yash <billionyash@gmail.com>
2021-03-03 17:35:16 +0530

    added two more tabs local and Capture

--- app/src/main/java/com/example/capture/MainActivity.java
+++ app/src/main/java/com/example/capture/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications ,R.id.navigation_capture , R.id.navigation_local)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
