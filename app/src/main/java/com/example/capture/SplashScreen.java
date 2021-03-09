e37f939d1e96dd330fb366a5ae892e00ec19afaf
billion-yash <billionyash@gmail.com>
2021-03-09 13:40:31 +0530

    ui changes

--- app/src/main/java/com/example/capture/SplashScreen.java
+++ app/src/main/java/com/example/capture/SplashScreen.java
@@ -14,7 +14,6 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         Context context = this;
         new Handler().postDelayed(new Runnable() {
             @Override
