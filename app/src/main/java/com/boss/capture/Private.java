01120e92b139b6183b4ea94fbabdb44cb088adfa
Yash dhanlobhe <ydhanlobhe1930@gmail.com>
2021-06-15 12:08:05 +0530

    key changes

--- app/src/main/java/com/boss/capture/Private.java
+++ app/src/main/java/com/boss/capture/Private.java
@@ -1,7 +1,7 @@
 package com.boss.capture;
 
 public class Private {
-
+//this are the test keys for unsplash API production keys will added while hosting app
     public  static String DeveloperAPI = "https://api.unsplash.com/";
     public static String APIKey = "client_id=z09b28_UcCHK5ULO8pE0jnBCYtErAnhR9Y_l3BkCKOI";
 
